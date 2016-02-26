dda465a819357c6dea199ca0f393f1a92adaa6d5
Pascal Crenzin <pascal.crenzin@sap.com>
2016-02-25 21:16:24 -0800

    back to beginning

--- app/todolist-controller.js
+++ app/todolist-controller.js
@@ -1,20 +1,3 @@
-function TodolistController($scope) {
-
-    $scope.name = "Pascal";
-    $scope.newTodo = "";
-    $scope.hideFinished = false;
-    $scope.todolist = [{
-        text: "learn angular",
-        finished: false,
-    }];
-
-    $scope.addTodo = function () {
-        $scope.todolist.push({
-            text: $scope.newTodo,
-            finished: false
-        });
-    };
-
-}
+function TodolistController($scope) {}
 
 angular.module("todoApp", ["ngMaterial"]).controller("TodolistController", TodolistController);
